10c6d3fe892f1d07081648c142a4d9ebfffb18b8
Aestimo K <thrivpro.agency@gmail.com>
2023-03-29 13:26:24 +0300

    Devise redirect on sign up and Devise views generated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+	before_action :devise_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def devise_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit( :username, :email, :password, :password_confirmation) }
+	end
+
+	# redirect to a specific page after sign up
+	def after_sign_in_path_for(resource)
+	  root_path
+	end
 end
